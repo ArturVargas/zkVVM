ed723583b44770152be6471cf0deabfa06eb3f84
Manuel Rivera <menu.rivera@gmail.com>
2026-02-20 21:45:13 -0600

    feat: MockVerifier.sol

---
+++ packages/contracts/MockVerifier.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {IVerifier} from './IVerifier.sol';
+
+contract MockVerifier is IVerifier {
+    function verify(bytes calldata, bytes32[] calldata) external pure override returns (bool) {
+        return true;
+    }
+}
